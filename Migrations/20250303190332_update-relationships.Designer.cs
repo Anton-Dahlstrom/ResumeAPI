2bd0c2a9e4ef5f2fd1e6c8434138d9b26723c9b3
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-03-04 00:09:52 +0100

    add migrations for relationship upgrade

--- Migrations/20250303190332_update-relationships.Designer.cs
+++ Migrations/20250303190332_update-relationships.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ResumeAPI.Data;
 
@@ -11,9 +12,11 @@ using ResumeAPI.Data;
 namespace ResumeAPI.Migrations
 {
     [DbContext(typeof(ResumeDBContext))]
-    partial class ResumeDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250303190332_update-relationships")]
+    partial class updaterelationships
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250303190332_update-relationships.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResumeAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class updaterelationships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Persons",
+                newName: "ID");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Jobs",
+                newName: "ID");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Educations",
+                newName: "ID");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PersonID_FK",
+                table: "Jobs",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PersonID_FK",
+                table: "Educations",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_PersonID_FK",
+                table: "Jobs",
+                column: "PersonID_FK");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Educations_PersonID_FK",
+                table: "Educations",
+                column: "PersonID_FK");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Educations_Persons_PersonID_FK",
+                table: "Educations",
+                column: "PersonID_FK",
+                principalTable: "Persons",
+                principalColumn: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Jobs_Persons_PersonID_FK",
+                table: "Jobs",
+                column: "PersonID_FK",
+                principalTable: "Persons",
+                principalColumn: "ID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Educations_Persons_PersonID_FK",
+                table: "Educations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Jobs_Persons_PersonID_FK",
+                table: "Jobs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Jobs_PersonID_FK",
+                table: "Jobs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Educations_PersonID_FK",
+                table: "Educations");
+
+            migrationBuilder.DropColumn(
+                name: "PersonID_FK",
+                table: "Jobs");
+
+            migrationBuilder.DropColumn(
+                name: "PersonID_FK",
+                table: "Educations");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Persons",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Jobs",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Educations",
+                newName: "Id");
+        }
+    }
+}
