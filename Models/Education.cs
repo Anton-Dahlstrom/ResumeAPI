d8967cd38499c0435c0e084ca575ae18ae67ecf2
Anton <anton.dahlstrom@hotmail.com>
2025-03-07 16:30:52 +0100

    make endDate nullable

--- Models/Education.cs
+++ Models/Education.cs
@@ -3,18 +3,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ResumeAPI.Models
 {
-	public class Education
-	{
-		[Key]
-		public int ID { get; set; }
-		public string School { get; set; }
-		public string Field { get; set; }
-		public string Description { get; set; }
-		public DateTime StartDate { get; set; }
-		public DateTime EndDate { get; set; }
+    public class Education
+    {
+        [Key]
+        public int ID { get; set; }
+        public string School { get; set; }
+        public string Field { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
 
-		[ForeignKey("Person")]
-		public int? PersonID_FK { get; set; }
-		public virtual Person Person { get; set; }
-	}
+        [ForeignKey("Person")]
+        public int? PersonID_FK { get; set; }
+        public virtual Person Person { get; set; }
+    }
 }
--- Models/Job.cs
+++ Models/Job.cs
@@ -3,18 +3,20 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ResumeAPI.Models
 {
-	public class Job
-	{
-		[Key]
-		public int ID { get; set; }
-		public string Company { get; set; }
-		public string Title { get; set; }
-		public string Description { get; set; }
-		public DateTime StartDate { get; set; }
-		public DateTime EndDate { get; set; }
+    public class Job
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Company { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [DataType(DataType.Date)]
+        public DateOnly StartDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateOnly? EndDate { get; set; }
 
-		[ForeignKey("Person")]
-		public int? PersonID_FK { get; set; }
-		public virtual Person Person { get; set; }
-	}
+        [ForeignKey("Person")]
+        public int? PersonID_FK { get; set; }
+        public virtual Person Person { get; set; }
+    }
 }
