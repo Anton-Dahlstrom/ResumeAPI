7fd72f54b8a937771b533853cfd565383c03ad0a
Anton <anton.dahlstrom@hotmail.com>
2025-03-07 16:35:09 +0100

    add educationGetDTO

---
+++ DTO/EducationGetDTO.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ResumeAPI.DTO
+{
+    public class EducationGetDTO
+    {
+        [MaxLength(256)]
+        public string School { get; set; }
+        [MaxLength(256)]
+        public string Field { get; set; }
+        [MaxLength(2500)]
+        public string Description { get; set; }
+        [DataType(DataType.Date)]
+        public DateOnly StartDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateOnly? EndDate { get; set; }
+    }
+}
